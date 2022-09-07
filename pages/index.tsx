4228345b6693ecaeaca4eef65911043978d33a8d
Winaya <winayaid@gmail.com>
2022-09-07 22:33:48 +0700

    fix: remove unused variabel

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 
-const Home: NextPage = ({ data }) => {
+const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <Head>
